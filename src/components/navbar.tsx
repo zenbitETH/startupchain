fe6c348820b5194b9006a3bff078f09870af6d24
habacuc.eth <57845110+HabacucMX@users.noreply.github.com>
2025-08-30 00:57:56 -0600

    Feat/branding review (#12)
    * white logo and color update
    
    * navbar update
    
    * business setup modal update
    
    * hero update
    
    * Branded animation
    
    * hovereffect
    
    * business setup update
    
    * ENS modals update
    
    * rocket animation update
    
    * margins on navbar
    
    * text size update

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -8,26 +8,26 @@ export function Navbar() {
   const { login, authenticated, user } = usePrivy()
 
   return (
-    <nav className="border-border/40 bg-background/80 relative z-50 h-16 border-b backdrop-blur-xl">
-      <div className="mx-auto h-full max-w-7xl px-4 sm:px-6 lg:px-8 py-5">
+    <nav className=" relative z-50 h-16 backdrop-blur-xl py-4">
+      <div className="mx-auto h-full max-w-7xl px-4 sm:px-6 lg:px-8 py-6">
         <div className="flex h-full items-center justify-between ">
-          <Link href="/" className="flex items-center space-x-2 py-5">
-              <Image src="/logo.svg" width={35} height={35} alt="StartUpChain Logo"/>
-            <span className="text-foreground text-3xl font-normal tracking-widest">
+          <Link href="/" className="flex items-center space-x-2 py-5 hover:text-primary">
+              <Image src="/logow.svg" width={35} height={35} alt="StartUpChain Logo"/>
+            <span className="text-foreground text-3xl font-normal tracking-widest hover:text-primary">
               StartUpChain
             </span>
           </Link>
           
           <div className="hidden items-center space-x-8 md:flex">
             {authenticated ? (
-              <div className="flex items-center space-x-4">
-                <span className="text-muted-foreground text-sm">
+              <div className="flex items-center space-x-4  ">
+                <span className="text-muted-foreground text-xl">
                   {user?.email?.address ||
                     user?.wallet?.address?.slice(0, 6) + '...'}
                 </span>
                 <Link
                   href="/dashboard"
-                  className="bg-primary text-primary-foreground hover:bg-primary/90 inline-block rounded-2xl px-4 py-2 font-medium transition-all duration-200"
+                  className="bg-primary text-background hover:text-white hover:bg-primary/90 text-2xl inline-block rounded-2xl px-4 py-2 font-medium transition-all duration-200"
                 >
                   Dashboard
                 </Link>
@@ -35,7 +35,7 @@ export function Navbar() {
             ) : (
               <button
                 onClick={login}
-                className="bg-primary text-primary-foreground hover:bg-primary/90 rounded-2xl px-4 py-2 font-medium transition-all duration-200"
+                className="cursor-pointer text-background hover:text-white bg-primary text-2xl hover:bg-primary/90 rounded-2xl px-4 py-2 font-medium transition-all duration-200"
               >
                 Login
               </button>
