19cc917c1d7135ce26896435ce5edaccb9af10ab
GertsDev <gertsdev@gmail.com>
2025-09-13 23:05:28 -0400

    feat(ui): enable scroll effect for animated rocket

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,8 @@ import { FeaturesSection } from '@/components/features-section'
 import { Footer } from '@/components/footer'
 import { HeroSection } from '@/components/hero-section'
 import { Navbar } from '@/components/navbar'
-import AnimatedRocket from '../components/AnimatedRocket'
+import AnimatedRocket from '@/components/AnimatedRocket/AnimatedRocket'
+
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
        <AnimatedRocket
         animated={true}
         speedLines={true}
+        scrollEffect={true}
         className="scroll-effect pointer-events-none fixed top-7 left-1/2 z-0 size-80 -translate-x-1/2 opacity-90 sm:size-150 lg:top-0 lg:size-160 xl:top-1/2 xl:right-0 xl:left-auto xl:size-200 xl:translate-x-20 xl:-translate-y-1/2"
       />
       <HeroSection />
