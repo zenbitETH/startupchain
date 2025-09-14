7f94fd55336edff87d5facbc612ac1edd2ff00ec
GertsDev <gertsdev@gmail.com>
2025-09-13 23:16:30 -0400

    refactor(ui): adjust animated rocket positioning in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,18 @@
+import AnimatedRocket from '@/components/AnimatedRocket/AnimatedRocket'
 import { FeaturesSection } from '@/components/features-section'
 import { Footer } from '@/components/footer'
 import { HeroSection } from '@/components/hero-section'
 import { Navbar } from '@/components/navbar'
-import AnimatedRocket from '@/components/AnimatedRocket/AnimatedRocket'
-
 
 export default function Home() {
   return (
     <div className="from-background via-background to-primary/5 relative min-h-screen overflow-hidden bg-gradient-to-br">
       <Navbar />
-       <AnimatedRocket
+      <AnimatedRocket
         animated={true}
         speedLines={true}
         scrollEffect={true}
-        className="scroll-effect pointer-events-none fixed top-7 left-1/2 z-0 size-80 -translate-x-1/2 opacity-90 sm:size-150 lg:top-0 lg:size-160 xl:top-1/2 xl:right-0 xl:left-auto xl:size-200 xl:translate-x-20 xl:-translate-y-1/2"
+        className="scroll-effect pointer-events-none fixed top-7 left-1/2 z-0 size-80 -translate-x-1/2 opacity-90 sm:size-150 lg:top-1/2 lg:right-0 lg:left-auto lg:size-160 lg:translate-x-20 lg:-translate-y-1/2 xl:size-200"
       />
       <HeroSection />
       <FeaturesSection />
