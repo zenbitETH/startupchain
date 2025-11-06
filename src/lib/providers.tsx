355eef4cdaee4f3f4132faf141862118e342cec8
GertsDev <gertsdev@gmail.com>
2025-11-05 22:16:54 -0500

    refactor: use privy shim in client providers

--- src/lib/providers.tsx
+++ src/lib/providers.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { PrivyProvider } from '@privy-io/react-auth'
+import { PrivyProvider } from '@/lib/privy'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { useEffect, useState } from 'react'
 import { WagmiProvider } from 'wagmi'
